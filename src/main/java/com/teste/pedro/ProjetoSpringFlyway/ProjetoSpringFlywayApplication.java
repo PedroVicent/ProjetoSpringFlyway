5192d9d9221ad990946ee73fba1f6ef916d4dde3
Pedro Bernardo <pedroh.vicent@gmail.com>
2023-09-14 13:43:27 -0300

    Project Commit

---
+++ src/main/java/com/teste/pedro/ProjetoSpringFlyway/ProjetoSpringFlywayApplication.java
@@ -0,0 +1,13 @@
+package com.teste.pedro.ProjetoSpringFlyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringFlywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringFlywayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teste/pedro/ProjetoSpringFlyway/ProjetoSpringFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.pedro.ProjetoSpringFlyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringFlywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
