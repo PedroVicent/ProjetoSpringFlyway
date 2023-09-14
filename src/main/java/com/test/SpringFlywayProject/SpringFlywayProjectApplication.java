e8a6bf6596db9da3ad10081fa132a7f5b3ebf8ad
Pedro Bernardo <pedroh.vicent@gmail.com>
2023-09-14 14:12:10 -0300

    Updating project name

--- src/main/java/com/test/SpringFlywayProject/SpringFlywayProjectApplication.java
+++ src/main/java/com/test/SpringFlywayProject/SpringFlywayProjectApplication.java
@@ -1,13 +1,13 @@
-package com.teste.pedro.ProjetoSpringFlyway;
+package com.test.SpringFlywayProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjetoSpringFlywayApplication {
+public class SpringFlywayProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjetoSpringFlywayApplication.class, args);
+		SpringApplication.run(SpringFlywayProjectApplication.class, args);
 	}
 
 }
